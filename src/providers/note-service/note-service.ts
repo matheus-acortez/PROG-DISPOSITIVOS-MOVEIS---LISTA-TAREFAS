73a9fa14513fe4f661584ad56472cf9f50585322
matheus-cortez99 <matheus.cortez99@gmail.com>
2020-05-30 16:59:07 -0300

    Teste salvando tarefa

--- src/providers/note-service/note-service.ts
+++ src/providers/note-service/note-service.ts
@@ -1,14 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
-/*
-  Generated class for the NoteServiceProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
+import { Storage } from '@ionic/storage';
 @Injectable()
-export class NoteServiceProvider {
+export class NoteService{
 
   constructor(public http: HttpClient) {
     console.log('Hello NoteServiceProvider Provider');
