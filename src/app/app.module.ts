1ee0af26db3e7d64da1a7739237c3fe6a54ccda3
matheus-cortez99 <matheus.cortez99@gmail.com>
2020-06-07 20:32:31 -0300

    Erros de importação em module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AdicionarPage } from '../pages/adicionar/adicionar';
 import { NoteService } from '../providers/note-service/note-service';
 import { IonicStorageModule } from '@ionic/storage';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ViewNotePage } from '../pages/view-note/view-note';
 
 
 
@@ -17,7 +18,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   declarations: [
     MyApp,
     HomePage,
-    AdicionarPage
+    AdicionarPage,
+    ViewNotePage
   ],
   imports: [
     BrowserModule,
@@ -30,7 +32,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   entryComponents: [
     MyApp,
     HomePage,
-    AdicionarPage
+    AdicionarPage,
+    ViewNotePage
   ],
   providers: [
     StatusBar,
